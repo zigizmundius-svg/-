a0a5ae8fb7797a719eb571ce8e86f37748632bf4
zigizmundius-svg <zigizmundius@gmail.com>
2026-07-10 13:46:00 +0300

    Update price-scraper.js

--- price-scraper.js
+++ price-scraper.js
@@ -109,6 +109,10 @@ async function scrapeWildberries(url) {
     headers: {
       'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0 Safari/537.36',
+      'Accept': 'application/json, text/plain, */*',
+      'Accept-Language': 'ru-RU,ru;q=0.9',
+      'Referer': 'https://www.wildberries.ru/',
+      'Origin': 'https://www.wildberries.ru',
     },
   });
 
